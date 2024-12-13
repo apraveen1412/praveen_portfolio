40c99bbe862223eac87198155517c08822dbaccf
Praveen Kumar <aaraveetipraveen@gmail.com>
2024-12-13 22:15:26 +0530

    Day 1

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('appearance').onclick=()=>{
+    const apElement = document.getElementById('ap');
+    const anchorTags = document.getElementsByTagName('a');
+  
+    if (apElement.style.color === 'white') { 
+      apElement.style.color = ''; // Reset to default color
+      apElement.style.backgroundColor = '';
+      for (let i = 0; i < anchorTags.length; i++) {
+        anchorTags[i].style.color = ''; 
+      }
+    } else {
+      apElement.style.color = 'white';
+      apElement.style.backgroundColor = 'black';
+      for (let i = 0; i < anchorTags.length; i++) {
+        anchorTags[i].style.color = 'gray';
+      }
+    }
+  }
