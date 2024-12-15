3c21433d3e96b2805d67c5afdb8d1ce74654118f
Praveen Kumar <aaraveetipraveen@gmail.com>
2024-12-15 13:02:58 +0530

    Day 3

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ document.getElementById('appearance').onclick=()=>{
 window.addEventListener('load', () => {
   document.querySelector('.land-intro').classList.add('loaded'); 
 });
+
+document.querySelector('.contact').onclick = (event) => {
+  event.preventDefault();
+  document.querySelector('.get-in-touch').scrollIntoView({
+    behavior:"smooth"
+  });
+}
+
+document.querySelector('.contact-mail').onclick = (event) => {
+  event.preventDefault(); // Prevent the default link behavior
+
+  function isMobileDevice() {
+      return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+  }
+
+  const email = "aaraveetipraveen@gmail.com";
+
+  if (isMobileDevice()) {
+      // On mobile, try to open the Gmail app or default email client with no subject or body
+      window.location.href = `mailto:${email}`;
+  } else {
+      // On desktop, redirect to Gmail web compose page with no subject or body
+      window.location.href = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`;
+  }
+};
