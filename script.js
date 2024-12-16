2f762d71ead096fd488c40092ae20f44b531d5f1
Praveen Kumar <aaraveetipraveen@gmail.com>
2024-12-17 00:18:51 +0530

    Day 3

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-document.getElementById('appearance').onclick=()=>{
-    const apElement = document.getElementById('ap');
-    const anchorTags = document.getElementsByTagName('a');
-  
-    if (apElement.style.color === 'white') { 
-      apElement.style.color = ''; // Reset to default color
-      apElement.style.backgroundColor = '';
-      for (let i = 0; i < anchorTags.length; i++) {
-        anchorTags[i].style.color = ''; 
+document.getElementById('appearance').onclick = () => {
+  const apElement = document.getElementById('ap');
+  const anchorTags = document.getElementsByTagName('a');
+  const apsub = document.querySelector('.ap-sub');
+  const paraTags = document.getElementsByTagName('p');
+  const moreBlogs = document.querySelector('.more-blogs');
+  const moreBlogsAnchor = document.querySelector('.more-blogs a');
+
+  const toggleColors = (elements, defaultColor, activeColor) => {
+      for (let i = 0; i < elements.length; i++) {
+          elements[i].style.color = elements[i].style.color === activeColor ? defaultColor : activeColor;
       }
-    } else {
+  };
+
+  // Toggle colors and backgrounds for apElement and anchorTags
+  if (apElement.style.color === 'white') {
+      apElement.style.color = ''; // Reset to default
+      apElement.style.backgroundColor = '';
+      moreBlogs.style.backgroundColor = 'rgb(31, 31, 31)';
+      moreBlogsAnchor.style.color = 'whitesmoke';
+  } else {
       apElement.style.color = 'white';
       apElement.style.backgroundColor = 'black';
-      for (let i = 0; i < anchorTags.length; i++) {
-        anchorTags[i].style.color = 'gray';
-      }
-    }
+      moreBlogs.style.backgroundColor = 'whitesmoke';
+      moreBlogsAnchor.style.color = 'rgb(31, 31, 31)';
   }
+  toggleColors(anchorTags, '', 'gray');
+
+  // Toggle colors and backgrounds for apsub and paraTags
+  if (apsub.style.color === 'whitesmoke') {
+      apsub.style.color = ''; // Reset to default
+      apsub.style.backgroundColor = '';
+  } else {
+      apsub.style.color = 'lightgray';
+      apsub.style.backgroundColor = 'rgb(31, 31, 31)';
+  }
+  toggleColors(paraTags, '', 'gray');
+};
+
+
+
+
 
 window.addEventListener('load', () => {
   document.querySelector('.land-intro').classList.add('loaded'); 
